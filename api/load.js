ef444d0711dd01758234d1ad3c71bc5f6286c345
thierryadine <thierry.adine@groupe-opera.com>
2026-04-19 15:41:34 +0400

    Update load.js

--- api/load.js
+++ api/load.js
@@ -1,14 +1,9 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  const FB_URL = 'https://crm-groupe-opera-default-rtdb.europe-west1.firebasedatabase.app/crm.json'
   try {
-    const response = await fetch('https://ybnafhfijzzcqmakfzy.supabase.co/rest/v1/crm_data?id=eq.groupe-opera&select=data', {
-      headers: {
-        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlibmFmaGZpamp6emNxbWFrZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MjYwODMsImV4cCI6MjA5MjEwMjA4M30.QCb5VPm40ZYN8V3-pZQnKav0s1gP39N4Pbn7sW0k0Hc',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlibmFmaGZpamp6emNxbWFrZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MjYwODMsImV4cCI6MjA5MjEwMjA4M30.QCb5VPm40ZYN8V3-pZQnKav0s1gP39N4Pbn7sW0k0Hc',
-      }
-    })
-    const rows = await response.json()
-    res.json(rows?.[0]?.data || null)
+    const response = await fetch(FB_URL)
+    const data = await response.json()
+    res.json(data)
   } catch(e) {
     res.status(500).json(null)
   }
